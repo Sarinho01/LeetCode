805290794b631e4ebe896ff9f85321c713308f14
Sarinho01 <alanvictor.sarinho@gmail.com>
2023-07-01 15:32:40 -0300

    add problem: Maximum Earnings From Taxi

--- src/medium/MaximumEarningsFromTaxi.java
+++ src/medium/MaximumEarningsFromTaxi.java
@@ -5,12 +5,13 @@ import java.util.Comparator;
 
 public class MaximumEarningsFromTaxi {
     public static void main(String[] args) {
+        MaximumEarningsFromTaxi maximumEarningsFromTaxi = new MaximumEarningsFromTaxi();
         int[][] rides = {{1,6,1},{3,10,2},{10,12,3},{11,12,2},{12,15,2},{13,18,1}};
-        System.out.println(maxTaxiEarnings(20,    rides));
+        System.out.println(maximumEarningsFromTaxi.maxTaxiEarnings(20,    rides));
 
 
     }
-    public static long maxTaxiEarnings(int n, int[][] rides) {
+    public long maxTaxiEarnings(int n, int[][] rides) {
         Arrays.sort(rides, Comparator.comparingInt(x -> x[1]));
         long[] maxPValues = new long[n];
         long maxValue = 0;
